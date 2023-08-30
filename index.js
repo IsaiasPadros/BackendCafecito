01bc686de5f210ed88b85528934368fd87be7bd1
unknown <isaiaspadros2@gmail.com>
2023-08-30 20:10:59 -0300

    Agregamos un puerto que funciona

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import express from 'express';
 const app = express();
 //crear una variable con express
 app.set('port', process.env.PORT || 4000 );
+app.listen(app.get('port'),()=>{
+    console.log('Estoy en el puerto ' + app.get('port'))
 
+})
 //2- middlewares
 
 //3- crear las rutas
