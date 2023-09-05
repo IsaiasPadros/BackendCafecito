cf149c37b8e9180a7419065acafd65ef28ba60e6
unknown <isaiaspadros2@gmail.com>
2023-09-04 21:21:12 -0300

    Agregamos productos a la base de dato

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,6 +1,6 @@
-import Producto from "../models/producto"
 
- 
+import Producto from "../models/producto.js"
+
 
 export const listarProductos= (req, res) =>{
 
@@ -12,7 +12,26 @@ try {
 } catch (error) {
     console.log(error)
 }
-
-
-
 }
+
+export const crearProducto= async(req, res) =>{
+try {
+    // ir a la base de datos y pedir los productos
+    //aqui los datos deberian estar validados
+    console.log(req.body)
+   
+    const productoNuevo = new Producto(req.body);
+    //guardar el productoNuevo en la base de datos
+    await productoNuevo.save();
+    res.status(201).json({
+        mensaje:'El producto fue creado correctamente'
+    })
+} catch (error) {
+    console.log(error);
+    res.status(400).json({
+        mensaje:'El producto no pudo ser creado'
+    })
+}
+}
+
+
